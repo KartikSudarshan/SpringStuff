777be3fc919c192f751e4cb26ddf6b3ba5204214
KartikSudarshan <kartiksudd@gmail.com>
2020-03-27 11:02:59 +0530

    additional files for controller

--- SpringMVCQuickStart/src/main/java/com/landonhotel/eventsapp/controller/QuoteRequestManagementController.java
+++ SpringMVCQuickStart/src/main/java/com/landonhotel/eventsapp/controller/QuoteRequestManagementController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.landonhotel.eventsapp.domain.QuoteRequest;
 
@@ -20,16 +22,43 @@ public class QuoteRequestManagementController {
 	public String listWeddingRequests() {
 		return "quoteRequestList";
 	}
-	@GetMapping(path = "/quoteRequests/{quoteId}", params = "eventType")
-	public String viewQuoteRequest(@PathVariable int quoteId) {
-		
-		// refer quoteId
-		return "quoteRequestDetails";
+
+	@GetMapping(path = "/quoteRequests/{quoteId}")
+	public ModelAndView viewQuoteRequest(@PathVariable int quoteId) {
+
+		QuoteRequest quoteRequestBean = new QuoteRequest();
+		quoteRequestBean.setBudget(5000);
+		quoteRequestBean.setEventType("wedding");
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("quoteRequestBean", quoteRequestBean);
+		mav.setViewName("quoteRequestDetails");
+		return mav;
 	}
+
+	
+	@GetMapping(path = "/quoteRequests/social/{id}")
+	public String viewRequestSocial(@PathVariable int id) {
+		String returnViewName="quoteRequestSocialEventDetailPage";
+		boolean someCondition=true;
+		if (someCondition)
+			returnViewName="redirect:/quoteRequest/wedding/{id}";
+		
+		return returnViewName;
+		
+	}
+	
+	@GetMapping(path = "/quoteRequests/social/{id}")
+	@ResponseBody
+	public QuoteRequest viewQuoteRequestApi(@PathVariable int id) {
+		QuoteRequest quoteRequest=new QuoteRequest();
+		return quoteRequest;
+		
+	}
+	
 	@PostMapping("/quoteDetail")
 	public String updateQuoteRequest(@ModelAttribute QuoteRequest formBean) {
-		//implement a save of all of the form bean information
-		
+		// implement a save of all of the form bean information
+
 		return "quoteRequestDetails";
 	}
 
